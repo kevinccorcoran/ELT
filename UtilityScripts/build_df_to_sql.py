de3a8019500c9d75f5f54aacba4debe2925c932f
kevinccorcoran <kevin.corcoran@hotmail.com>
2024-01-15 10:33:45 +0100

    wip

---
+++ UtilityScripts/build_df_to_sql.py
@@ -0,0 +1,55 @@
+# Working script 11/16
+
+import pandas as pd 
+import yfinance as yf
+import os
+import json
+from sqlalchemy import create_engine
+from contextlib import suppress
+import sys
+
+import logging
+try:
+    #tickers = ['VIX','^GSPC','GD=F','CL=F','GC=F','^TNX', 'ZC=F','PA=F','ZS=F','LBS=F','ZW=F','HG=F']    
+    tickers = ['^GSPC']
+
+    #tickers = ['ABNB']
+    def build_df(tickers):
+        df = pd.DataFrame(yf.download(tickers[0], 
+                        #start='2000-01-01',
+                        #end='2022-12-31',
+                        progress='True'))
+        df.rename(columns={ 'Date':'DATE',
+                        'Open':'OPEN',
+                        'High':'HIGH',
+                        'Low':'LOW',
+                        'Close':'CLOSE',
+                        'Adj Close':'ADJCLOSE',
+                        'Volume':'VOLUME'}, inplace=True)
+        df.insert(0, 'TICKER', tickers[0])
+        for ticker in tickers[1:]:
+            dx =  pd.DataFrame(yf.download(ticker, 
+                        #start='2000-01-01',
+                        #end='2022-12-31',
+                        progress='True'))
+            dx.insert(0, 'TICKER', ticker)
+            dx.rename(columns={ 'Date':'DATE',
+                        'Open':'OPEN',
+                        'High':'HIGH',
+                        'Low':'LOW',
+                        'Close':'CLOSE',
+                        'Adj Close':'ADJCLOSE',
+                        'Volume':'VOLUME'}, inplace=True)
+            df = pd.concat([df, dx])
+        return df
+
+    result = build_df(tickers) 
+
+    print(result)
+
+    engine = create_engine('postgresql://postgres:9356@localhost:5433/QA')        
+    result.to_sql('HISTORICAL_DAILY_MASTER_STAGING', engine, if_exists='replace', schema='STATS') 
+
+
+except Exception as e:
+    logging.NOTSET(e, exc_info=True)  # log exception info at CRITICAL log level
