6f5097c6ef83c05be80acc577e591aa5f28a19fb
kevinccorcoran <kevin.corcoran@hotmail.com>
2024-02-13 16:25:41 +0100

    Kan 6 build df to sql should be updated (#7)
    * wip
    
    * wip

--- UtilityScripts/build_df_to_sql.py
+++ UtilityScripts/build_df_to_sql.py
@@ -38,7 +38,7 @@ def save_to_database(df, table_name, connection_string):
         logging.exception("Failed to save data to database")
 
 if __name__ == "__main__":
-    tickers = ['^GSPC']  # Example ticker
+    tickers = ['^GSPC','ME']  # Example ticker
     table_name = 'historical_daily_master'
     connection_string = 'postgresql://postgres:9356@localhost:5433/cp'
     
@@ -48,55 +48,3 @@ if __name__ == "__main__":
         save_to_database(result_df, table_name, connection_string)
     except Exception as e:
         logging.exception("Unexpected error occurred")
-=======
-from contextlib import suppress
-import sys
-
-import logging
-try:
-    # tickers = ['VIX','^GSPC','GD=F','CL=F','GC=F','^TNX', 'ZC=F','PA=F','ZS=F','LBS=F','ZW=F','HG=F']
-    tickers = ['^GSPC']
-
-    # tickers = ['ABNB']
-    def build_df(tickers):
-        df = pd.DataFrame(yf.download(tickers[0],
-                                      # start='2000-01-01',
-                                      # end='2022-12-31',
-                                      progress='True'))
-        df.rename(columns={'Date': 'date',
-                           'Open': 'open',
-                           'High': 'high',
-                           'Low': 'low',
-                           'Close': 'close',
-                           'Adj Close': 'adjclose',
-                           'Volume': 'volume'}, inplace=True)
-        df.insert(0, 'TICKER', tickers[0])
-        for ticker in tickers[1:]:
-            dx = pd.DataFrame(yf.download(ticker,
-                                          # start='2000-01-01',
-                                          # end='2022-12-31',
-                                          progress='True'))
-            dx.insert(0, 'TICKER', ticker)
-            dx.rename(columns={'Date': 'date',
-                               'Open': 'open',
-                               'High': 'high',
-                               'Low': 'low',
-                               'Close': 'close',
-                               'Adj Close': 'adjclose',
-                               'Volume': 'volume'}, inplace=True)
-            df = pd.concat([df, dx])
-        return df
-
-    result = build_df(tickers)
-
-    print(result)
-
-    engine = create_engine('postgresql://postgres:9356@localhost:5433/QA')
-    result.to_sql('historical_daily_master_staging', engine,
-                  if_exists='replace', schema='stats')
-
-
-except Exception as e:
-    # log exception info at CRITICAL log level
-    logging.NOTSET(e, exc_info=True)
-
