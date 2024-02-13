613abcca9b15fa0a3ae31467c1e4a953e66086c3
kevinccorcoran <kevin.corcoran@hotmail.com>
2024-02-13 15:49:28 +0100

    Feature/kan 8 create historical daily master (#3) (#5)
    * build df update
    
    * test

--- UtilityScripts/build_df_to_sql.py
+++ UtilityScripts/build_df_to_sql.py
@@ -1,10 +1,54 @@
-# Working script 11/16
-
 import pandas as pd
 import yfinance as yf
-import os
-import json
 from sqlalchemy import create_engine
+
+import logging 
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def build_df(tickers):
+    try:
+        df = pd.DataFrame()
+        for ticker in tickers:
+            dx = yf.download(ticker, progress=True)
+            dx.rename(columns={'Open': 'open',
+                               'High': 'high',
+                               'Low': 'low',
+                               'Close': 'close',
+                               'Adj Close': 'adj_close',
+                               'Volume': 'volume'}, inplace=True)
+            dx['ticker'] = ticker  # Add ticker column to the DataFrame
+            if df.empty:
+                df = dx
+            else:
+                df = pd.concat([df, dx])
+        df.reset_index(inplace=True)  # Reset index to make 'Date' a column
+        return df
+    except Exception as e:
+        logging.exception("Error building DataFrame")
+        raise
+
+def save_to_database(df, table_name, connection_string):
+    try:
+        engine = create_engine(connection_string)
+        df.to_sql(table_name, engine, if_exists='replace', schema='raw', index=False)
+        logging.info(f"Data successfully saved to {table_name}")
+    except Exception as e:
+        logging.exception("Failed to save data to database")
+
+if __name__ == "__main__":
+    tickers = ['^GSPC']  # Example ticker
+    table_name = 'historical_daily_master'
+    connection_string = 'postgresql://postgres:9356@localhost:5433/cp'
+    
+    try:
+        result_df = build_df(tickers)
+        print(result_df)
+        save_to_database(result_df, table_name, connection_string)
+    except Exception as e:
+        logging.exception("Unexpected error occurred")
+=======
 from contextlib import suppress
 import sys
 
@@ -55,3 +99,4 @@ try:
 except Exception as e:
     # log exception info at CRITICAL log level
     logging.NOTSET(e, exc_info=True)
+
