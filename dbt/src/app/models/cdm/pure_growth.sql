e529c6f22eb1cad85e649c2c657d318d19e2b0fc
kevinccorcoran <kevin.corcoran@hotmail.com>
2024-03-08 13:05:51 +0100

    finished

--- dbt/src/app/models/cdm/pure_growth.sql
+++ dbt/src/app/models/cdm/pure_growth.sql
@@ -1,28 +1,47 @@
+{{ 
+    config(
+        materialized='table', 
+        unique_key='ticker'
+    )
 
--- Use the `ref` function to select from other models
- 
-WITH BASE_TABLE AS
-	(SELECT date, TICKER,
-			OPEN,
-			HIGH,
-			LOW,
-			CLOSE,
-			ADJ_CLOSE,
-			VOLUME,
-			PROCESSED_AT
-		FROM {{ source('raw', 'historical_daily_main') }} ),
-		
- MIN_MAX_DATE AS
-	(SELECT TICKER,
-			MIN(BT.DATE) AS MIN_DATE_OPEN,
-			MAX(BT.DATE) AS MAX_DATE_close
-		FROM BASE_TABLE BT WHERE TICKER = 'MSFT'
+}}
 
-		GROUP BY TICKER)
-SELECT date, bt.ticker, open, close
-FROM BASE_TABLE BT
-JOIN MIN_MAX_DATE MMD ON (BT.TICKER = MMD.TICKER
-AND BT.DATE = MMD.MIN_DATE_OPEN ) 
-or ( BT.TICKER = MMD.TICKER
-AND BT.DATE = MAX_DATE_close )
---where id = 1
+WITH BASE_TABLE AS (
+    SELECT
+        DATE,
+        REGEXP_REPLACE(TICKER, '[^a-zA-Z]', '', 'g') AS TICKER,
+        OPEN,
+        HIGH,
+        LOW,
+        CLOSE,
+        ADJ_CLOSE,
+        VOLUME,
+        PROCESSED_AT
+    FROM {{ source('raw', 'historical_daily_main') }}
+),
+
+MIN_MAX_DATE AS (
+    SELECT
+        TICKER,
+        MIN(DATE)::date AS MIN_DATE,
+        MAX(DATE)::date AS MAX_DATE
+    FROM BASE_TABLE
+
+    GROUP BY TICKER
+)
+
+SELECT
+    MMD.MIN_DATE,
+    MMD.MAX_DATE,
+    MMD.TICKER,
+    MMD.MAX_DATE - MMD.MIN_DATE AS AGE,
+    ROUND(BT.OPEN::numeric, 2) AS OPEN,
+    ROUND(BT2.CLOSE::numeric, 2) AS CLOSE
+FROM MIN_MAX_DATE AS MMD
+INNER JOIN BASE_TABLE AS BT
+    ON
+        MMD.TICKER = BT.TICKER
+        AND MMD.MIN_DATE = BT.DATE
+INNER JOIN BASE_TABLE AS BT2 ON
+    MMD.TICKER = BT2.TICKER
+    AND MMD.MAX_DATE = BT2.DATE
