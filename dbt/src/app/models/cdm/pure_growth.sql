2c565bfa089592be7c558f318205fc1b7cd2cf2c
kevinccorcoran <kevin.corcoran@hotmail.com>
2024-03-08 18:21:51 +0100

    Kan 16 pure growth.sql building (#19)
    * done
    
    * fix

--- dbt/src/app/models/cdm/pure_growth.sql
+++ dbt/src/app/models/cdm/pure_growth.sql
@@ -1,3 +1,4 @@
+
 {{ 
     config(
         materialized='table', 
@@ -5,43 +6,77 @@
     )
 
 }}
-
-WITH BASE_TABLE AS (
-    SELECT
-        DATE,
-        REGEXP_REPLACE(TICKER, '[^a-zA-Z]', '', 'g') AS TICKER,
-        OPEN,
-        HIGH,
-        LOW,
-        CLOSE,
-        ADJ_CLOSE,
-        VOLUME,
-        PROCESSED_AT
-    FROM {{ source('raw', 'historical_daily_main') }}
+-- Initial setup: Select and clean data from the "historical_daily_main" table.
+WITH base AS (
+    SELECT 
+        DATE(DATE) AS date,
+        REGEXP_REPLACE(TICKER, '[^a-zA-Z]', '', 'g') AS ticker, -- Remove non-alphabetic characters from ticker.
+        ROUND(CAST(OPEN AS NUMERIC), 2) AS open, -- Round OPEN to 2 decimal places.
+        ROUND(CAST(HIGH AS NUMERIC), 2) AS high, -- Round HIGH to 2 decimal places.
+        ROUND(CAST(LOW AS NUMERIC), 2) AS low, -- Round LOW to 2 decimal places.
+        ROUND(CAST(CLOSE AS NUMERIC), 2) AS close, -- Round CLOSE to 2 decimal places.
+        ROUND(CAST(ADJ_CLOSE AS NUMERIC), 2) AS adj_close, -- Round ADJ_CLOSE to 2 decimal places.
+        VOLUME
+    FROM 
+        {{ source('raw', 'historical_daily_main') }}
+        
+--     WHERE 
+--         TICKER IN ('MSFT', '^GSPC') -- Filter for specific tickers.
 ),
 
-MIN_MAX_DATE AS (
-    SELECT
-        TICKER,
-        MIN(DATE)::date AS MIN_DATE,
-        MAX(DATE)::date AS MAX_DATE
-    FROM BASE_TABLE
+-- Create a subset for baseline ticker data.
+index_base AS (
+    SELECT * 
+    FROM base 
+    WHERE ticker = 'GSPC'
+),
 
-    GROUP BY TICKER
+-- Select the earliest date record for each ticker excluding the baseline.
+MIN_TICKER_DATE AS (
+    SELECT *
+    FROM (
+        SELECT
+            date,
+            ticker,
+            open,
+            high,
+            low,
+            volume,
+            ROW_NUMBER() OVER (PARTITION BY ticker ORDER BY date) AS row_number
+        FROM base
+        WHERE ticker NOT IN (SELECT DISTINCT ticker FROM index_base)
+    ) subquery
+    WHERE row_number = 1
+),
+
+-- Select the latest date record for each ticker excluding the baseline.
+MAX_TICKER_DATE AS (
+    SELECT *
+    FROM (
+        SELECT
+            date,
+            ticker,
+            open,
+            close,
+            high,
+            low,
+            volume,
+            ROW_NUMBER() OVER (PARTITION BY ticker ORDER BY date DESC) AS row_number
+        FROM base
+        WHERE ticker NOT IN (SELECT DISTINCT ticker FROM index_base)
+    ) subquery
+    WHERE row_number = 1
 )
 
-SELECT
-    MMD.MIN_DATE,
-    MMD.MAX_DATE,
-    MMD.TICKER,
-    MMD.MAX_DATE - MMD.MIN_DATE AS AGE,
-    ROUND(BT.OPEN::numeric, 2) AS OPEN,
-    ROUND(BT2.CLOSE::numeric, 2) AS CLOSE
-FROM MIN_MAX_DATE AS MMD
-INNER JOIN BASE_TABLE AS BT
-    ON
-        MMD.TICKER = BT.TICKER
-        AND MMD.MIN_DATE = BT.DATE
-INNER JOIN BASE_TABLE AS BT2 ON
-    MMD.TICKER = BT2.TICKER
-    AND MMD.MAX_DATE = BT2.DATE
+-- Final selection
+SELECT 
+    ARRAY[mitd.date, matd.date] AS dates, -- Array of minimum and maximum dates.
+    ARRAY[ib.ticker, mitd.ticker] AS tickers, -- Array of index and ticker symbols.
+    ARRAY[ib.open, mitd.open] AS opens, -- Array of opening prices for index and minimum date.
+    ARRAY[ib2.close, matd.close] AS closes -- Array of closing prices for index and maximum date.
+FROM 
+    MIN_TICKER_DATE mitd
+    JOIN index_base ib ON mitd.date = ib.date
+    JOIN MAX_TICKER_DATE matd ON mitd.ticker = matd.ticker
+    JOIN index_base ib2 ON matd.date = ib2.date
+-- Optional filters and ordering can be added here.
