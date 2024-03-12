a129e578fe9708a81c12fa34274dce3365a7db77
kevinccorcoran <kevin.corcoran@hotmail.com>
2024-03-12 17:24:59 +0100

    fix

--- dbt/src/app/models/cdm/pure_growth.sql
+++ dbt/src/app/models/cdm/pure_growth.sql
@@ -9,25 +9,16 @@
 -- Initial setup: Select and clean data from the "historical_daily_main" table.
 WITH base AS (
     SELECT 
-        DATE(DATE) AS date,
-        REGEXP_REPLACE(TICKER, '[^a-zA-Z]', '', 'g') AS ticker, -- Remove non-alphabetic characters from ticker.
-        ROUND(CAST(OPEN AS NUMERIC), 2) AS open, -- Round OPEN to 2 decimal places.
-        ROUND(CAST(HIGH AS NUMERIC), 2) AS high, -- Round HIGH to 2 decimal places.
-        ROUND(CAST(LOW AS NUMERIC), 2) AS low, -- Round LOW to 2 decimal places.
-        ROUND(CAST(CLOSE AS NUMERIC), 2) AS close, -- Round CLOSE to 2 decimal places.
-        ROUND(CAST(ADJ_CLOSE AS NUMERIC), 2) AS adj_close, -- Round ADJ_CLOSE to 2 decimal places.
-        VOLUME
-    FROM 
-        {{ source('raw', 'historical_daily_main') }}
-        
---     WHERE 
---         TICKER IN ('MSFT', '^GSPC') -- Filter for specific tickers.
+        *
+    FROM
+        {{ ref('historical_daily_main_clean') }}
 ),
 
 -- Create a subset for baseline ticker data.
 index_base AS (
     SELECT * 
     FROM base 
+    WHERE ticker IN ('GSPC')
 ),
 
 -- Select the earliest date record for each ticker excluding the baseline.
