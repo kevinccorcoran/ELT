fa18ccdb807909887e0411b73b32d017b106874e
kevinccorcoran <kevin.corcoran@hotmail.com>
2024-12-20 00:34:47 +0100

    WIP

--- gunicorn_config.py
+++
@@ -1,6 +0,0 @@
-# gunicorn_config.py
-import os
-
-bind = "0.0.0.0:{}".format(os.getenv("PORT", 8000))  # Bind to Heroku's port
-workers = 2  # Reduce worker count to fit Heroku's resource limits
-timeout = 60  # Ensure app starts within Heroku's boot timeout limit
