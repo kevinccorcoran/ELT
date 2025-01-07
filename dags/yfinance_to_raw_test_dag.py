4cf31bfc44efa2ff9cb3dad3e0628188a8fb3c05
kevinccorcoran <kevin.corcoran@hotmail.com>
2025-01-07 13:47:40 +0100

    first try using DATABASE_URL

--- dags/yfinance_to_raw_test_dag.py
+++ dags/yfinance_to_raw_test_dag.py
@@ -1,41 +1,37 @@
-from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 import os
+from datetime import datetime, timedelta
 
-# Retrieve environment-specific variables
-env = os.getenv("ENV", "staging")  # Default to staging
+env = os.getenv("ENV", "staging")
 database_url = os.getenv("DATABASE_URL", "")
 
 if not database_url:
     raise ValueError("DATABASE_URL is not set in the Heroku config vars or environment")
 
-# Optional: Check if DATABASE_URL requires modification for SQLAlchemy
+# Convert 'postgres://' to 'postgresql+psycopg2://'
 if database_url.startswith("postgres://"):
     database_url = database_url.replace("postgres://", "postgresql+psycopg2://", 1)
 
-# Define the default arguments for the DAG
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2023, 1, 1),  # Start date to support a wide backfill range if needed
+    'start_date': datetime(2023, 1, 1),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 0,
     'retry_delay': timedelta(minutes=5),
 }
 
-# Define the DAG
 dag = DAG(
     dag_id="yfinance_to_raw_test_dag",
     default_args=default_args,
     description="DAG to run a Python script that updates raw.",
-    schedule_interval=None,  # Run only when manually triggered
-    catchup=False,  # Ensures it does not backfill from start_date to now
+    schedule_interval=None,
+    catchup=False,
 )
 
-# Task to run the yfinance_to_raw_etl.py Python script
 fetch_yfinance_data = BashOperator(
     task_id='fetch_yfinance_data',
     bash_command=(
@@ -45,19 +41,18 @@ fetch_yfinance_data = BashOperator(
         '/app/.heroku/python/bin/python3 /app/python/src/dev/raw/yfinance_to_raw_etl.py '
         '--start_date "1950-01-01" --end_date "{{ ds }}"'
     ),
+    # Pass in the environment variables exactly as the script expects
     env={
         'DB_CONNECTION_STRING': database_url,
-        'ENV': env,
+        'ENV': env,  # optional if your script also needs 'ENV'
     },
     dag=dag,
 )
 
-# Task to trigger the next DAG for creating the cdm.fibonacci_transform_dates table
 trigger_api_cdm_data_ingestion = TriggerDagRunOperator(
     task_id='trigger_dag_for_cdm_api_cdm_data_ingestion_table',
-    trigger_dag_id="raw_to_api_cdm_data_ingestion_dag",  # ID of the DAG to trigger
+    trigger_dag_id="raw_to_api_cdm_data_ingestion_dag",
     dag=dag,
 )
 
-# Set task dependencies
 fetch_yfinance_data >> trigger_api_cdm_data_ingestion
