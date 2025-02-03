b257b2946c9daccf8d7faeee848daf0cf5981023
kevinccorcoran <kevin.corcoran@hotmail.com>
2025-02-03 17:02:42 +0100

    WIP

--- dags/cdm_company_cagr_dag.py
+++ dags/cdm_company_cagr_dag.py
@@ -33,6 +33,7 @@ def get_dbt_bash_command(env: str, db_connection_string: str) -> Tuple[str, Dict
     """
     if env == "heroku_postgres":
         bash_command = (
+            f'echo "Current DB: $DATABASE_URL" && '
             f'export PYTHONPATH=$PYTHONPATH:/app/python/src && '
             f'cd /app/dbt && '
             f'(dbt run --models company_cagr ; exit 0) '
