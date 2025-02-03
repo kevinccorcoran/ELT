fd8341c75148da17a982c3a7c6ec830f01c70e5e
kevinccorcoran <kevin.corcoran@hotmail.com>
2025-02-03 18:07:17 +0100

    WIP

--- dags/cdm_company_cagr_dag.py
+++ dags/cdm_company_cagr_dag.py
@@ -36,8 +36,8 @@ def get_dbt_bash_command(env: str, db_connection_string: str) -> Tuple[str, Dict
             'echo "Current DB: $DATABASE_URL" && '
             'export PYTHONPATH=$PYTHONPATH:/app/python/src && '
             'cd /app/dbt && '
-            '/app/.heroku/python/bin/dbt debug && '
-            '(/app/.heroku/python/bin/dbt run --models company_cagr --debug ; exit 0) 2>&1 | tee /tmp/dbt_cagr_output.log'
+            '/app/.heroku/python/bin/dbt debug --profiles-dir /app/.dbt && '
+            '(/app/.heroku/python/bin/dbt run --models company_cagr --debug --profiles-dir /app/.dbt ; exit 0) 2>&1 | tee /tmp/dbt_cagr_output.log'
         )
         # bash_command = (
         #     f'echo "Current DB: $DATABASE_URL" && '
