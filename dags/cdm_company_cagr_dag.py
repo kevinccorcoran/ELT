945e5e12e32cce8f4e8160cd41d4412b6ca43909
kevinccorcoran <kevin.corcoran@hotmail.com>
2025-02-12 20:26:37 +0100

    WIP

--- dags/cdm_company_cagr_dag.py
+++ dags/cdm_company_cagr_dag.py
@@ -35,7 +35,7 @@ def get_dbt_bash_command(env: str, db_connection_string: str) -> Tuple[str, Dict
     """
     if env == "heroku_postgres":
         env_vars = {
-            "ENV": Variable.get("ENV", default_var="heroku_postgres"),  # Ensure ENV is set
+            #"ENV": Variable.get("ENV", default_var="heroku_postgres"),  # Ensure ENV is set
             "DB_HOST": Variable.get("DB_HOST"),
             "DB_PORT": Variable.get("DB_PORT"),
             "DB_USER": Variable.get("DB_USER"),
