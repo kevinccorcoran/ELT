10e29d6667e0795a47270b449eab1cacae22df86
kevinccorcoran <kevin.corcoran@hotmail.com>
2025-03-07 17:52:35 +0100

    WIP

--- python/src/dev/raw/yfinance_to_raw_etl.py
+++ python/src/dev/raw/yfinance_to_raw_etl.py
@@ -84,7 +84,9 @@ def build_df(tickers, start_date=None, end_date=None):
         # df['date'] = df['date'].astype(str)
 
         #df['date'] = pd.to_datetime(df['date']).dt.date  # yields Python `datetime.date` objects
-        
+        # Ensure date column is correctly formatted before inserting into the database
+        df['date'] = pd.to_datetime(df['date']).dt.tz_localize(None).dt.date
+        df = df.dropna(subset=["date"])
         # Add the processed_at column with the current timestamp
         df['processed_at'] = datetime.now()
 
